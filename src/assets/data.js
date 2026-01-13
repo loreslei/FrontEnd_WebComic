05a99693fe4a1c65b6c9b832ad6064ecf9fb656d
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2026-01-13 20:24:25 -0300

    bg mais escuro para combinar

--- src/assets/data.js
+++ src/assets/data.js
@@ -39,7 +39,7 @@ let sonho = [
         nome: "Filha",
         url_imagem: '/images/beds/daughter.png',
         quadrinho: ['/images/daughter/01.jpeg', '/images/daughter/02.jpg'],
-        color_bg: 'bg-[#c6bcf8]',
+        color_bg: 'bg-[#c6bcf810]',
         color_btn: 'bg-[#8772f3]',
         color_btn_hover: 'hover:bg-[#6E57E8]',
         svg: padroes.headphones,
