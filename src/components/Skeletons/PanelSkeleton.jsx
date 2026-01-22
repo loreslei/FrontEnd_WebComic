3e89bd1d1e1d33be2bcd32f5d0cc0bc7ea5d9f32
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2026-01-22 12:13:23 -0300

    correcao p telas largas

--- src/components/Skeletons/PanelSkeleton.jsx
+++ src/components/Skeletons/PanelSkeleton.jsx
@@ -1,5 +1,5 @@
 const PanelSkeleton = () => (
-  <div className="w-[90%] aspect-3/5 bg-zinc-700/40 rounded-lg animate-pulse my-3" />
+  <div className="w-[90%] lg:w-[60%] aspect-3/5 bg-zinc-700/40 rounded-lg animate-pulse my-3" />
 );
 
 export default PanelSkeleton;
