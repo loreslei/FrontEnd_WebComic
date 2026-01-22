cc03b707b0b15c6ffdadf997547d14c0d47155b9
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2026-01-22 12:05:50 -0300

    teste com vh

--- src/components/Skeletons/PanelSkeleton.jsx
+++ src/components/Skeletons/PanelSkeleton.jsx
@@ -1,5 +1,5 @@
 const PanelSkeleton = () => (
-  <div className="w-[90%] h-[90%] bg-zinc-700/40 rounded-lg animate-pulse my-3" />
+  <div className="w-[90%] h-[70vh] bg-zinc-700/40 rounded-lg animate-pulse my-3" />
 );
 
 export default PanelSkeleton;
